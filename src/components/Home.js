80c5960c0a782322035b2b784e9c21442ce56545
zmeyer44 <meyerz@wustl.edu>
2020-05-02 21:03:37 -0400

    mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,35 +8,38 @@ export default class Home extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-6  mx-auto mt-5 zoom">
-            <img src="../img/HutchMeyer.png" width="500px"></img>
+          <div className="col-10 col-md-6  mx-auto mt-5 zoom">
+            <img src="../img/HutchMeyer.png" max-width="500px"></img>
           </div>
         </div>
 
-        <div className="row align-items-center mt10 pt-5 mb-5">
-          <ButtonContainer2 className="px-4 ml-5 py-1">
-            <Link to="/stats" className="ml-auto my-0 align-items-center">
-              <div className="row align-items-center text-blue">
-                <span className="mr-2">
-                  <i className="fas fa-search fa-2x"></i>
-                </span>
-                Track a stock
-              </div>
-            </Link>
-          </ButtonContainer2>
+        <div className="row mt10 align-items-center">
 
-          <div className="col-5"></div>
-
-          <ButtonContainer2 className="px-4 button2 py-1">
-            <Link to="/details" className="ml-auto align-items-center">
-              <div className="row align-items-center text-blue">
-                <span className="mr-2">
-                  <i className="fas fa-play-circle fa-2x"></i>
-                </span>
-                Learn About Stocks
+        <div className="  mx-auto">
+          <Link to="/stats" className="mx-auto my-0 align-items-center">
+            <ButtonContainer2 className="px-4">
+                <div className="row align-items-center text-center">
+              <span className="mr-2">
+                <i className="fas fa-search fa-2x"></i>
+              </span>
+              Track a stock
               </div>
-            </Link>
-          </ButtonContainer2>
+            </ButtonContainer2>
+          </Link>
+          </div>
+            <div className="col-md-3 col-0"></div>
+            <div className=" mx-auto">
+          <Link to="/details" className="mx-auto my-0 align-items-center">
+            <ButtonContainer2 className="px-4">
+                <div className="row align-items-center text-center">
+              <span className="mr-2">
+                <i className="fas fa-play-circle fa-2x"></i>
+              </span>
+              Learn about stocks
+              </div>
+            </ButtonContainer2>
+          </Link>
+          </div>
         </div>
 
         <hr className="mt=5" />
